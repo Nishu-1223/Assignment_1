0c335bf48dcf24dd5f8db96a50433fc63ca7d4f0
Nishu-1223 <nishu.jain0033@gmail.com>
2025-02-25 18:36:50 +0530

    Add files via upload

---
+++ Assignment1_part01.py
@@ -0,0 +1,15 @@
+#Takes two numbers as input from the user.
+#Performs the basic mathematical operations on these two numbers
+
+first_num=int(input("Enter the first number: "))
+second_num=int(input("Enter the second number: "))
+
+Add= first_num+second_num
+Substration= first_num-second_num
+Multiplication= first_num*second_num
+Division= first_num/second_num
+
+print(f"Addition: {Add}")
+print(f"Substration: {Substration}")
+print(f"Multiplication: {Multiplication}")
+print(f"Division: {Division}")
