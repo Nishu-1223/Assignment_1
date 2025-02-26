e6959ce9cd613740226ec6aaed4d0212fd6dbe36
Nishu <nishu@Nishus-MacBook-Air.local>
2025-02-26 14:25:29 +0530

    Rename repo name and removed assignment_2

--- Assignment_2/Task01.py
+++
@@ -1,9 +0,0 @@
-# Task 1: Check if a Number is Even or Odd
-
-number=int(input("Enter a number: "))
-if number%2==0:
-    print(f"{number} is an even number.")
-else:
-    print(f"{number} is an odd number.")
-
-
